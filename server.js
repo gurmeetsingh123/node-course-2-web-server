7e637c57457f578488a22cee0d943f1426db167b
gurmeet singh <gurmeetsingh5617@gmail.com>
2018-06-14 18:22:19 +0530

    adding project page

--- server.js
+++ server.js
@@ -48,6 +48,14 @@ app.get('/about', (req, res) => {
 
 	});
 });
+
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs',{
+		pagetitle: 'project page'
+
+	});
+});
+
  app.get('/bad', (req, res) =>{
  	res.send({
  		error: 'error occured'
